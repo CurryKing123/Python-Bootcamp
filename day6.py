fe9dd7c4ec86be6d91dfab698a047d68c04469c3
CurryKing123 <isaac.park@hotmail.com>
2025-03-26 17:07:17 -0400

    remove daily practice

--- day6.py
+++
@@ -1,8 +0,0 @@
-def my_function():
-    i = 0
-    while i < 3:
-        print(i)
-        i += 1
-
-
-my_function()
--- day8.py
+++
@@ -1,9 +0,0 @@
-# def greet_name(name):
-#     print(f"Hello {name}")
-
-# greet_name("John")
-
-def greet_name_location(name, location):
-    print(f"Hello {name}, welcome to {location}!")
-
-greet_name_location("Bob", "Valhalla")
